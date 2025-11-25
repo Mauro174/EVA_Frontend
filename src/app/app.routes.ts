b9780164bdbf4823301bed71db64312be528d5ed
Mauricio <u201910755@upc.edu.pe>
2025-11-25 00:58:15 -0500

    backup temporal de cambios antes de mover a rama Mauricio

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { Usuario } from './components/usuario/usuario';
+import { Landing } from './components/landing/landing';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: Landing},
+  {
+    path: 'usuarios',
+    component: Usuario,
+    children: [
+    ],
+  }
+];
