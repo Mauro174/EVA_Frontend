1d0a19534430263bf665b101e1e01b423ea18d44
Rosy <u202313849@upc.edu.pe>
2025-11-29 15:12:27 -0500

    Actualizar rutas para integrar las vistas de conversaciones

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -7,6 +7,15 @@ import { UsuarioInsert } from './components/usuario/usuario-insert/usuario-inser
 import { seguridadGuard } from './guard/seguridad-guard';
 import { Relacionesusuarios } from './components/relacionesusuarios/relacionesusuarios';
 import { RelacionesusuariosInsert } from './components/relacionesusuarios/relacionesusuarios-insert/relacionesusuarios-insert';
+import { Conversaciones } from './components/conversaciones/conversaciones';
+import { ConversacionesInsert } from './components/conversaciones/conversaciones-insert/conversaciones-insert';
+//import { ReporteConversacionesCanal } from './components/conversaciones/reporte-conversaciones-canal/reporte-conversaciones-canal';
+//import { ReporteConversacionesUsuario } from './components/conversaciones/reporte-conversaciones-usuario/reporte-conversaciones-usuario';
+import { Medicamento } from './components/medicamento/medicamento';
+import { MedicamentoInsert } from './components/medicamento/medicamento-insert/medicamento-insert';
+import { MedicamentoList } from './components/medicamento/medicamento-list/medicamento-list';
+
+
 
 export const routes: Routes = [
 
@@ -31,6 +40,26 @@ export const routes: Routes = [
       { path: 'relacionesusuarios', component: Relacionesusuarios },
       { path: 'relacionesusuarios/nuevo', component: RelacionesusuariosInsert },
       { path: 'relacionesusuarios/edits/:id', component: RelacionesusuariosInsert },
+
+      {
+        path: 'medicamentos',
+        component: Medicamento,
+        children: [
+          { path: '', component: MedicamentoList },
+          { path: 'news', component: MedicamentoInsert },
+          { path: 'edits/:id', component: MedicamentoInsert },
+        ],
+      },
+
+      // 🔹 CONVERSACIONES (TODO EN PLURAL)
+      {
+        path: 'conversaciones',
+        component: Conversaciones,
+        children: [
+          { path: 'nuevo', component: ConversacionesInsert },      // /conversaciones/nuevo
+          { path: 'edits/:id', component: ConversacionesInsert },  // /conversaciones/edits/1
+        ],
+      },
 
     ]
   }
